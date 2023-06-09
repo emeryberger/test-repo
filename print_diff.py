217e5a3ddc69a11e82f7a4519fd8892e35641d3a
Emery Berger <emery.berger@gmail.com>
2023-06-09 16:58:25 -0400

    Test.

--- print_diff.py
+++ print_diff.py
@@ -1,38 +1,28 @@
+import pandas as pd
 import os
-import requests
+import sys
 
-def get_diff(owner, repo, commit_sha, base_ref=None):
-    url = f"https://api.github.com/repos/{owner}/{repo}/compare"
-    print(f"url = {url}")
-    params = {
-        "base": base_ref or commit_sha,
-        "head": commit_sha
-    }
-    headers = {"Accept": "application/vnd.github.v3.diff"}
+def print_csv_diffs():
+    head_commit = os.getenv('GITHUB_SHA')
+    base_commit = os.getenv('GITHUB_BASE_REF')
+    files_changed_command = f"git diff --name-only {base_commit} {head_commit}"
+    files_changed = os.popen(files_changed_command).read().splitlines()
 
-    response = requests.get(url, params=params, headers=headers)
-    response.raise_for_status()
-    return response.text
+    csv_files = [file for file in files_changed if file.endswith('.csv')]
+    if not csv_files:
+        print('No CSV files changed.')
+        return
 
-def print_diff(diff):
-    print("Commit Diff:")
-    print("------------")
-    print(diff)
+    for file in csv_files:
+        print(f'Diffs for {file}:')
+        print('-' * 80)
+        base_file_command = f'git show {base_commit}:{file}'
+        head_file_command = f'git show {head_commit}:{file}'
+        base_df = pd.read_csv(os.popen(base_file_command), header=None)
+        head_df = pd.read_csv(os.popen(head_file_command), header=None)
+        diff_df = head_df.compare(base_df)
+        print(diff_df)
+        print()
 
-def main():
-    owner = os.environ.get("GITHUB_REPOSITORY_OWNER")
-    repo = os.environ.get("GITHUB_REPOSITORY")
-    event_name = os.environ.get("GITHUB_EVENT_NAME")
-
-    if event_name == "pull_request":
-        pull_request_number = os.environ.get("GITHUB_PULL_REQUEST_NUMBER")
-        diff = get_diff(owner, repo, f"refs/pull/{pull_request_number}/merge")
-    else:
-        commit_sha = os.environ.get("GITHUB_SHA")
-        base_ref = os.environ.get("GITHUB_BASE_REF")
-        diff = get_diff(owner, repo, commit_sha, base_ref)
-
-    print_diff(diff)
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    print_csv_diffs()
