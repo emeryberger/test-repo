bd3f6314ed317eb594385f3e4469fe9663cfe871
Emery Berger <emery.berger@gmail.com>
2023-06-12 14:48:57 -0400

    Test.

---
+++ json_to_diff.py
@@ -0,0 +1,19 @@
+def process():
+    import sys
+    json_data = sys.argv[1]
+    
+    data = json.loads(json_data)
+
+    # Extract file path
+    file_path = data['files'][0]['path']
+
+    # Extract changed lines
+    changed_lines = [change['content'] for change in data['files'][0]['chunks'][0]['changes']
+                     if change['type'] in ('AddedLine', 'DeletedLine')]
+
+    # Print the changed lines
+    for line in changed_lines:
+        print(line)
+
+if __name__ == "__main__":
+    process()
